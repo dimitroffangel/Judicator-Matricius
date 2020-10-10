d81649bbb1226a2f50f9f072cab64ea1992d1764
dimitroffangel <angeldimitrov05@gmail.com>
2020-10-10 16:20:05 +0300

    Added a triangulation way, which increases the speed to O(rows * cols)

--- Main.cpp
+++ Main.cpp
@@ -143,10 +143,112 @@ GaussEliminationAnswer&& DoGaussElimination(std::vector<std::vector<double> > ma
 	return std::move(stateAnswer);
 }
 
-void FindMoreAnswers(const std::vector<double>& matrix, std::vector<std::vector<double>>& answers,
-	const std::vector<double> valuesToCheck)
+GaussEliminationAnswer&& TriangulateGaussElimination(std::vector<std::vector<double> > matrix, std::vector<double>& answer)
 {
+	const int numberOfRows = matrix.size();
+	const int numberOfCols = matrix[0].size() - 1;
 
+	std::vector<int> pivotIndices(numberOfCols, -1);
+
+	// find the row with the biggest absolute value on col position 
+	for (int col = 0, row = 0; col < numberOfCols && row < numberOfRows; ++col)
+	{
+		int bestCurrentRow = row;
+
+		for (int i = row; i < numberOfRows; ++i)
+		{
+			if (std::abs(matrix[i][col]) > std::abs(matrix[bestCurrentRow][col]))
+			{
+				bestCurrentRow = i;
+			}
+		}
+
+		// if the best row with the max heurestic is less than ERROR_BOUND - then assume it is a zero there
+		if (std::abs(matrix[bestCurrentRow][col]) < ERROR_BOUND)
+		{
+			continue;
+		}
+
+		// swap that matrix[currentRow] with matrix[row]
+		for (int i = col; i <= numberOfCols; ++i)
+		{
+			std::swap(matrix[bestCurrentRow][i], matrix[row][i]);
+		}
+
+		pivotIndices[col] = row;
+
+		for (int i = row + 1; i < numberOfRows; ++i)
+		{
+			double c = matrix[i][col] / matrix[row][col];
+
+			for (int j = col; j <= numberOfCols; ++j)
+			{
+				matrix[i][j] -= matrix[row][j] * c;
+			}
+		}
+
+		++row;
+	}
+
+	assert(answer.size() == 0);
+
+	answer.assign(numberOfCols, 1);
+	std::bitset<MAX_NUMBER_OF_ROWS> foundAnswers;
+
+	for (int i = numberOfCols - 1; i >= 0; --i)
+	{
+		if (pivotIndices[i] != -1)
+		{
+			foundAnswers[i] = 1;
+
+			double sum = 0;
+			for (size_t j = i + 1; j < numberOfCols; ++j)
+			{
+				if (j == i)
+				{
+					continue;
+				}
+
+				sum += matrix[pivotIndices[i]][j] * answer[j];
+			}
+
+			answer[i] = (matrix[pivotIndices[i]][numberOfCols] - sum) / matrix[pivotIndices[i]][i];
+		}
+	}
+
+	for (size_t i = 0; i < numberOfRows; ++i)
+	{
+		double sum = 0;
+
+		for (int j = 0; j < numberOfCols; ++j)
+		{
+			sum += answer[j] * matrix[i][j];
+		}
+
+		if (std::abs(sum - matrix[i][numberOfCols]) > ERROR_BOUND)
+		{
+			GaussEliminationAnswer stateAnswer;
+			stateAnswer.stateAfterElimination = NO_ANSWER_CODE;
+			stateAnswer.matrix = std::move(matrix);
+			return std::move(stateAnswer);
+		}
+	}
+
+	for (size_t i = 0; i < numberOfCols; ++i)
+	{
+		if (answer[i] == 1 && !foundAnswers[i])
+		{
+			GaussEliminationAnswer stateAnswer;
+			stateAnswer.stateAfterElimination = MULTIPLE_ANSWER_CODE;
+
+			stateAnswer.matrix = std::move(matrix);
+			return std::move(stateAnswer);
+		}
+	}
+
+	GaussEliminationAnswer stateAnswer;
+	stateAnswer.matrix = std::move(matrix);
+	return std::move(stateAnswer);
 }
 
 int main()
@@ -175,7 +277,8 @@ int main()
 	}
 
 	std::vector<double> answer;
-	GaussEliminationAnswer&& res = std::move(DoGaussElimination(matrix, answer));
+	//GaussEliminationAnswer&& res = std::move(DoGaussElimination(matrix, answer));
+	GaussEliminationAnswer&& res = std::move(TriangulateGaussElimination(matrix, answer));
 
 	return 0;
 }
